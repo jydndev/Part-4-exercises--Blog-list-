6799ec276ecca1acdaf837d9ee4ab79aa0e9a8fe
jydnbach <jaydenkim95@gmail.com>
2025-04-23 14:31:23 +0700

    test: favorite blog

--- tests/list_helper.test.js
+++ tests/list_helper.test.js
@@ -26,3 +26,45 @@ describe('total likes', () => {
     assert.strictEqual(result, 5);
   });
 });
+
+const allBlogs = [
+  {
+    _id: '5a422a851b54a676234d17f7',
+    title: 'React patterns',
+    author: 'Michael Chan',
+    url: 'https://reactpatterns.com/',
+    likes: 7,
+    __v: 0,
+  },
+  {
+    _id: '5a422aa71b54a676234d17f8',
+    title: 'Go To Statement Considered Harmful',
+    author: 'Edsger W. Dijkstra',
+    url: 'https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf',
+    likes: 5,
+    __v: 0,
+  },
+  {
+    _id: '5a422b3a1b54a676234d17f9',
+    title: 'Canonical string reduction',
+    author: 'Edsger W. Dijkstra',
+    url: 'http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html',
+    likes: 12,
+    __v: 0,
+  },
+];
+
+describe('favorite blog', () => {
+  test('returns blog with most likes', () => {
+    const result = listHelper.favoriteBlog(allBlogs);
+
+    assert.deepStrictEqual(result, {
+      _id: '5a422b3a1b54a676234d17f9',
+      title: 'Canonical string reduction',
+      author: 'Edsger W. Dijkstra',
+      url: 'http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html',
+      likes: 12,
+      __v: 0,
+    });
+  });
+});
--- utils/list_helper.js
+++ utils/list_helper.js
@@ -6,7 +6,16 @@ const totalLikes = (blogs) => {
   return blogs.reduce((sum, blog) => sum + blog.likes, 0);
 };
 
+const favoriteBlog = (blogs) => {
+  if (blogs.length === 0) {
+    return null;
+  }
+
+  return blogs.reduce((max, blog) => (blog.likes > max.likes ? blog : max));
+};
+
 module.exports = {
   dummy,
   totalLikes,
+  favoriteBlog,
 };
