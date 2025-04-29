0242f8865ec5303c67cadeffb7cd9a44c90d3bbd
jydnbach <jaydenkim95@gmail.com>
2025-04-29 07:33:08 +0700

    4.9

--- tests/blog_api.test.js
+++ tests/blog_api.test.js
@@ -1,17 +1,27 @@
 const { test, after } = require('node:test');
+const assert = require('node:assert');
 const mongoose = require('mongoose');
 const supertest = require('supertest');
 const app = require('../app');
+const Blog = require('../models/blog');
 
 const api = supertest(app);
 
-test('blogs returned as json', async () => {
-  api
+test('blogs are returned as json', async () => {
+  await api
     .get('/api/blogs')
     .expect(200)
-    .expect('Contet-Type', /applicatoin\/json/);
+    .expect('Content-Type', /application\/json/);
+});
 
-  after(async () => {
-    await mongoose.connection.close();
-  });
+test('blog posts have id property instead of _id', async () => {
+  const response = await api.get('/api/blogs');
+  const blog = response.body[0];
+
+  assert.strictEqual(blog.id !== undefined, true);
+  assert.strictEqual(blog._id === undefined, true);
+});
+
+after(async () => {
+  await mongoose.connection.close();
 });
